e3127e296c31045d1e4c1b8391eae57b049cd1c6
velsreis <victoremmanuelreis@gmail.com>
2022-04-08 12:02:17 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,130 @@
+console.log('ferramentas ok');
+
+const tmi = require('tmi.js');
+const nome_bot = 'botmudavidas'
+const canal = 'beatrrriste'
+const token = 'oauth:sr5wmer3yuv0ikzh63opelaxdti97d'
+
+const opts = {
+  identity: {
+    username: nome_bot,
+    password: token
+  },
+  channels: [canal]
+};
+
+const array = ['bairro', 'bistro', 'amparo']; // palavras aleatorias do letreco
+
+var box = {   //armazena os quadrados do letreco
+  box1: '🟥',
+  box2: '🟥',
+  box3: '🟥',
+  box4: '🟥',
+  box5: '🟥',
+  box6: '🟥'
+};
+
+
+//intercepta mensagem do chat
+function mensagemChegou(alvo, contexto, mensagem, ehBot) {
+  if (ehBot) {
+    return; //se for mensagens do nosso bot ele não faz nada
+  }
+  const nomeDoComando = mensagem.trim();// remove espaço em branco da mensagem para verificar o comando
+  // checando o nosso comando
+ switch(contexto['message-type']) {
+  case "chat":
+  if (nomeDoComando === '!letreco') {      //tudo quebrado so to usando esse if pra guardar oq ja fiz
+
+    const letreco = array[Math.floor(Math.random() * array.length)];
+    console.log(letreco);
+
+    if (mensagem[0] === letreco[0]) {
+      box.box1 = '🟩';
+    } else {
+      box.box1 = '🟥';
+    };
+    // seta segunda caixa 
+    if (mensagem[1] === letreco[1]) {
+      box.box2 = '🟩';
+    } else {
+      box.box2 = '🟥';
+    };
+    //////////////
+    if (mensagem[2] === letreco[2]) {
+      box.box3 = '🟩';
+    } else {
+      box.box3 = '🟥';
+    };
+    //////////////
+    if (mensagem[3] === letreco[3]) {
+      box.box4 = '🟩';
+    } else {
+      box.box4 = '🟥';
+    };
+    //////////////
+    if (mensagem[4] === letreco[4]) {
+      box.box5 = '🟩';
+    } else {
+      box.box5 = '🟥';
+    };
+    //////////////
+    if (mensagem[5] === letreco[5]) {
+      box.box6 = '🟩';
+    } else {
+      box.box6 = '🟥';
+    };
+    if (mensagem || mensagem.length === 6) {
+      client.say(canal, box.box1 + ' ' + box.box2 + ' ' + box.box3 + ' ' + box.box4 + ' ' + box.box5 + ' ' + box.box6);
+
+    };
+
+
+  }
+
+  if (nomeDoComando === '!ola') {
+    client.say(alvo, 'Olá Mundo!');
+  }
+
+
+  if (nomeDoComando === '!parar') {
+    client.say(alvo, "ok");
+    client.disconnect();
+  }
+  break;
+  case "whisper":
+    console.log(`sussuro recebido ${mensagem}`);
+  
+  
+    if(mensagem === "!zap"){   // quebrado nao avalia se o usuario é ou nao sub e falta conseguir fazer o cliente enviar o link por whisper
+      console.log(contexto.username);
+      if(contexto.subscriber === true){
+        console.log('usuario é sub enviar link')
+      } else {
+        console.log('desculpa vi aqui que vc nao é sub')
+      }
+      
+    }
+    break;
+}
+}
+
+function entrouNoChatDaTwitch(endereco, porta) {
+  console.log(`* Bot entrou no endereço ${endereco}:${porta}`);
+}
+
+// Cria um cliente tmi com  nossas opções
+const client = new tmi.client(opts);
+
+// Registra nossas funções
+client.on('message', mensagemChegou);
+client.on('connected', entrouNoChatDaTwitch);
+
+client.on("subscription", (alvo, username, method, message, userstate) => {
+    client.say(` Hey @${username} obrigado por se inscrever, se quiser pode me mandar !zap no sussurro que lhe mandarei o link do grupo do canal`);
+  
+  });
+
+
+// Connecta na Twitch:
+client.connect();
